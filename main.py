a05b28b8c89211489ffc9f8033c7aba91fdd109b
Tim <tim.r.burr@gmail.com>
2023-12-02 18:29:13 -0700

    works, as long as the spellings match in both spreadsheets

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ master_dict = {}
 # Last Name,First Name,Grade Level,Teacher
 with open('All Students.csv') as fp:
     for line in fp:
-        print(line)
-        # todo read master list into map("first,last": data)
+        # print(line)
+        line = line.strip()
+        pieces = line.split(",")
+        key = pieces[1].casefold() + pieces[0].casefold()
+        master_dict[key] = line
 
 # Last Name,First Name
 with open('Club Students.csv') as fp:
     for line in fp:
-        print(line)
-        # todo: create key like first,last
-        thisKey = "f,l"
-        master_dict.pop(thisKey)
+        #print(line)
+        line = line.strip()
+        pieces = line.split(",")
+        thisKey = pieces[1].casefold() + pieces[0].casefold()
+        if thisKey in master_dict:
+            master_dict.pop(thisKey)
 
 for key in master_dict:
     # todo: write to file instead
     print(master_dict[key])
+    pass
